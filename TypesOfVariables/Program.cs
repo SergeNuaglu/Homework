275e9b41e9b794a1a0c44b6f8d9f0861c11c40ed
SergeNuaglu <serge.nuaglu@gmail.com>
2022-01-27 19:23:36 +0200

    variable declaration

---
+++ TypesOfVariables/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TypesOfVariables
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
