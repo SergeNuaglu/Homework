d1ffe48acff7af25cdf29d6bc67f70b408d457df
SergeNuaglu <serge.nuaglu@gmail.com>
2022-01-27 20:45:50 +0200

    completed the task

---
+++ RowsOfPictures/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace RowsOfPictures
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int picturesCount = 52;
+            int picturesInRow = 3;
+            int filledRow;
+            int picturesOverflow;
+
+            filledRow = picturesCount / picturesInRow;
+            picturesOverflow = picturesCount % picturesInRow;
+
+            Console.WriteLine("Количество заполненных рядов: " + filledRow);
+            Console.WriteLine("Количество картинок сверх меры: " + picturesOverflow);
+        }
+    }
+}
