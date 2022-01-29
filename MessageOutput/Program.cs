36d6b25f3bd1490bbcf34a65538093b1db2345dc
SergeNuaglu <serge.nuaglu@gmail.com>
2022-01-29 20:47:06 +0200

    task is done

---
+++ MessageOutput/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MessageOutput
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string message;
+            int outputsCount;
+
+            Console.Write("Введите сообщение, которое хотите вывести: ");
+            message = Console.ReadLine();
+            Console.Write("Введите количество выводов: ");
+            outputsCount = Convert.ToInt32(Console.ReadLine());
+
+            while (outputsCount-- > 0)
+            {
+                Console.WriteLine(message);
+            }
+        }
+    }
+}
