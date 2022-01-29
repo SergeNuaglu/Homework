7cf83ff27e7a36baeb125d77e27a87d57e57ca61
SergeNuaglu <serge.nuaglu@gmail.com>
2022-01-29 19:49:33 +0200

    Task is done

---
+++ InTheCinic/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace InTheCinic
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int timeForOnePerson = 10;
+            int peopleCount;
+            int waitingTime;
+            int hours;
+            int minutes;
+
+            Console.Write("Введите кол-во старушек: ");
+            peopleCount = Convert.ToInt32(Console.ReadLine());
+
+            waitingTime = peopleCount * timeForOnePerson;
+            minutes = waitingTime % 60;
+            hours = (waitingTime - minutes) / 60;
+
+            Console.WriteLine($"Вы должны отстоять в очереди {hours} часа и {minutes} минут.");
+        }
+    }
+}
