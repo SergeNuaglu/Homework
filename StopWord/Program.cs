8132254e48b160be55ad646bd77aa473274949f1
SergeNuaglu <serge.nuaglu@gmail.com>
2022-01-29 21:17:38 +0200

    solved the problem

---
+++ StopWord/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace StopWord
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string stopWord = "exit";
+            string userWord;
+
+            Console.Write("Чтобы завершить программу введите exit: ");
+            userWord = Console.ReadLine();
+
+            while (userWord != stopWord)
+            {
+                Console.Write("Не то! Введите exit: ");
+                userWord = Console.ReadLine();
+            }
+
+            Console.WriteLine("Программа завершена!");
+        }
+    }
+}
+
