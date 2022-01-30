32b28090b506d6c1d9f8279ffdda0c677f768534
SergeNuaglu <serge.nuaglu@gmail.com>
2022-01-30 13:17:10 +0200

    created project Polyclinic and solved task

---
+++ Polyclinic/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Polyclinic
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int timeForOnePerson = 10;
+            int oldWomenCount;
+            int waitingTime;
+            int hour;
+            int minute;
+
+            Console.Write("Введите кол-во старушек: ");
+            oldWomenCount = Convert.ToInt32(Console.ReadLine());
+
+            waitingTime = oldWomenCount * timeForOnePerson;
+            minute = waitingTime % 60;
+            hour = (waitingTime - minute) / 60;
+
+            Console.WriteLine($"Вы должны отстоять в очереди {hour} часа и {minute} минут.");
+        }
+    }
+}
