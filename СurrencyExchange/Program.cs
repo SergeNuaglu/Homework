8d3050b62aa562d16088af43643ac6e002a1d6c9
SergeNuaglu <serge.nuaglu@gmail.com>
2022-01-30 13:08:18 +0200

    deleted project InTheCinic

---
+++ СurrencyExchange/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace СurrencyExchange
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            float rubCount;
+            float usdCount;
+            float euroCount;
+
+            Console.WriteLine("Ваш баланс:");
+            Console.Write("Рубли - ");
+            rubCount = Convert.ToSingle(Console.ReadLine());
+            Console.Write("Доллары - ");
+            usdCount = Convert.ToSingle(Console.ReadLine());
+            Console.Write("Евро - ");
+            euroCount = Convert.ToSingle(Console.ReadLine());
+
+            Console.WriteLine("Добро пожаловать в обменник валют!\nЗдесь можно обменять 3 валюты: рубли, доллары и евро.\nЧто продаете?");
+            Console.Write("Что продаете?");
+        }
+    }
+}
