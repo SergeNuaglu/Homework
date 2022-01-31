c6c5898413aeef46415c1e29a05eb92c6421777a
SergeNuaglu <98533549+SergeNuaglu@users.noreply.github.com>
2022-01-31 20:55:55 +0200

    Update Program.cs

--- Polyclinic/Program.cs
+++ Polyclinic/Program.cs
@@ -7,18 +7,20 @@ namespace Polyclinic
         public static void Main(string[] args)
         {
             int peopleCount;
-            int minutesOfWaiting;
+            int minutesPerPerson = 10;
+            int totalMinutes;
             int hoursBeforeAdmission;
             int minutesBeforeAdmission;
 
             Console.Write("Введите кол-во старушек: ");
             peopleCount = Convert.ToInt32(Console.ReadLine());
 
-            minutesOfWaiting = peopleCount * 10;
-            hoursBeforeAdmission = minutesOfWaiting / 60;
-            minutesBeforeAdmission = minutesOfWaiting % 60;
+            totalMinutes = peopleCount * minutesPerPerson;
+            hoursBeforeAdmission = totalMinutes / 60;
+            minutesBeforeAdmission = totalMinutes % 60;
 
             Console.WriteLine($"Вы должны отстоять в очереди {hoursBeforeAdmission} часа и {minutesBeforeAdmission} минут.");
         }
     }
 }
+
