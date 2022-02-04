b036ff64faf953f0001b6e7c3dc84b9fc44c8e26
SergeNuaglu <serge.nuaglu@gmail.com>
2022-02-04 20:02:31 +0200

    fixed Arrey

--- Arrey/Program.cs
+++ Arrey/Program.cs
@@ -13,24 +13,27 @@ namespace Arrey
             };
             int sum = 0;
             int product = 1;
+            int rowToAdd = 2;
+            int columnToMultiply = 1;
 
             for (int i = 0; i < numbers.GetLength(0); i++)
             {
                 for (int j = 0; j < numbers.GetLength(1); j++)
                 {
                     Console.Write(numbers[i, j] + " ");
-
-                    if (i == 1)
-                    {
-                        sum += numbers[i, j];
-                    }
-
-                    if (j == 0)
-                    {
-                        product *= numbers[i, j];
-                    }
                 }
+
                 Console.WriteLine();
+            }
+
+            for (int i = 0; i < numbers.GetLength(1); i++)
+            {
+                sum += numbers[rowToAdd - 1, i];
+            }
+
+            for (int i = 0; i < numbers.GetLength(0);i++)
+            {
+                product *= numbers[columnToMultiply - 1, i];
             }
 
             Console.WriteLine("Сумма второй строки: " + sum +
---
+++ LargestElement/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace LargestElement
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int[,] numbers = new int [10, 10];
+
+
+        }
+    }
+}
