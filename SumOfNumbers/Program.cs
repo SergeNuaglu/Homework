502cc239444f69af10d96f43385cd70b4fbe2b53
SergeNuaglu <serge.nuaglu@gmail.com>
2022-02-05 16:29:48 +0200

    Solved task

--- SumOfNumbers/Program.cs
+++ SumOfNumbers/Program.cs
@@ -9,32 +9,64 @@ namespace SumOfNumbers
             float[] numbers = new float[0];
             string[] commands = { "sum", "exit" };
             float sum = 0;
-            string userInput;
+            string operation = null;
+            string userInput = null;
             bool isThisCommand = false;
             bool isAppWork = true;
 
             while (isAppWork)
             {
-                Console.Clear();
-                Console.SetCursorPosition(0, 6);
+                Console.Clear();       
+                Console.SetCursorPosition(0, 8);
                 Console.WriteLine("Команды:\nsum - сложить числа\nexit - выйти из программы\n\n");
+                Console.SetCursorPosition(0, 5);
+
+                if (userInput != "sum" && userInput != null)
+                {
+                    Console.Write("Операция сложения: " + operation);
+                }
+                else if (userInput == "sum")
+                {
+                    Console.Write("Операция сложения: " + operation + " = " + sum);
+                    operation = null;
+                    sum = 0;
+                    Console.WriteLine("\nНажмите любую клавишу для новой операции...");
+                    Console.ReadKey();
+                }
+
                 Console.SetCursorPosition(0, 0);
                 Console.WriteLine("Введите число или команду: ");
                 userInput = Console.ReadLine();
-                float[] tempNumbers = new float[numbers.Length + 1];
 
-                for (int i = 0; i < commands.Length; i++)
+
+                for (int i = 0; i < commands.Length && isThisCommand == false; i++)
                 {
                     if (userInput == commands[i])
                     {
                         isThisCommand = true;
-                        break;
                     }
                 }
 
                 if (isThisCommand == false)
                 {
+                    float[] tempNumbers = new float[numbers.Length + 1];
                     tempNumbers[tempNumbers.Length - 1] = Convert.ToSingle(userInput);
+                    sum += tempNumbers[tempNumbers.Length - 1];
+
+                    if (operation == null)
+                    {
+                        operation += userInput;
+                    }
+                    else
+                    {
+                        operation += " + " + userInput;
+                    }
+
+                    for (int i = 0; i < numbers.Length; i++)
+                    {
+                        tempNumbers[i] = numbers[i];
+                    }
+
                     numbers = tempNumbers;
                 }
                 else
@@ -44,17 +76,11 @@ namespace SumOfNumbers
                         case "sum":
                             if (numbers.Length >= 2)
                             {
-                                for (int i = 0; i < numbers.Length; i++)
-                                {
-                                    sum += numbers[i];    
-                                }
-
-                                Console.WriteLine("Сумма чисел = " + sum);
-                                Console.WriteLine("Нажмите любую клавишу для новой операции...");
-                                Console.ReadKey();
+                                numbers = new float[0];
                             }
                             else
                             {
+                                userInput = " ";
                                 Console.WriteLine("Для сложения нужно как минимум два числа!");
                                 Console.WriteLine("Нажмите любую клавишу для того, чтобы попробовать еще...");
                                 Console.ReadKey();
@@ -63,13 +89,10 @@ namespace SumOfNumbers
                         case "exit":
                             isAppWork = false;
                             break;
-                        default:
-                            Console.WriteLine("Такой команды нет!");
-                            break;
                     }
+
+                    isThisCommand = false;
                 }
-                Console.SetCursorPosition(0, 2);
-                Console.Write("Операция: ");
             }
         }
     }
