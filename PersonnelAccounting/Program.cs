0ebade8e9a89ea0a77e81df18423c9e7f3a2bc8c
SergeNuaglu <serge.nuaglu@gmail.com>
2022-02-07 19:07:31 +0200

    fixed

--- PersonnelAccounting/Program.cs
+++ PersonnelAccounting/Program.cs
@@ -6,8 +6,10 @@ namespace PersonnelAccounting
     {
         public static void Main(string[] args)
         {
-            string[] fullNames = new string[1];
-            string[] positions = new string[1];
+            string[] fullNames = new string[0];
+            string[] positions = new string[0];
+            string fullName;
+            string position;
             string userInput;
             bool isAppWorking = true;
 
@@ -22,50 +24,53 @@ namespace PersonnelAccounting
                 switch (userInput)
                 {
                     case "1":
-                        Console.Write("ФИО: ");
-                        fullNames = AddDossier(fullNames, Console.ReadLine());
-                        Console.Write("Должность: ");
-                        positions = AddDossier(positions, Console.ReadLine());
+                        AddDossier(ref fullNames, ref positions, out fullName, out position);
                         break;
                     case "2":
                         OutputDossiers(fullNames, positions);
                         break;
                     case "3":
-                        Console.Write("Номер досье: ");
-                        int dossierNumber = Convert.ToInt32(Console.ReadLine());
-                        fullNames = DeleteDossier(dossierNumber, fullNames);
-                        positions = DeleteDossier(dossierNumber, positions);
+                        DeleteDossier(ref fullNames, ref positions);
                         break;
                     case "4":
-                        Console.Write("Фамилия: ");
-                        string lastname = Console.ReadLine();
-                        OutputDossiers(fullNames, positions, lastname);
+                        Console.WriteLine(SearchByLastname(fullNames, positions));
                         break;
                     case "5":
                         isAppWorking = false;
                         break;
                 }
+
+                Console.ReadKey();
             }
         }
 
-        static string[] AddDossier(string[] arrey, string value)
+        static void AddDossier(ref string[] fullNames, ref string[] positions, out string fullName, out string position)
         {
-            if (arrey[0] == null)
+            Console.Write("ФИО: ");
+            fullName = Console.ReadLine();
+            Console.Write("Должность: ");
+            position = Console.ReadLine();
+
+            if (fullNames == null)
             {
-                arrey[0] = value;
-                return arrey;
+                fullNames[0] = fullName;
+                positions[0] = position;
             }
             else
             {
-                string[] tempArrey = new string[arrey.Length + 1];
+                string[] tempFullNames = new string[fullNames.Length + 1];
+                string[] tempPositions = new string[fullNames.Length + 1];
 
-                for (int i = 0; i < arrey.Length; i++)
+                for (int i = 0; i < fullNames.Length; i++)
                 {
-                    tempArrey[i] = arrey[i];
-                    tempArrey[tempArrey.Length - 1] = value;
+                    tempFullNames[i] = fullNames[i];
+                    tempPositions[i] = positions[i];
                 }
 
-                return tempArrey;
+                tempFullNames[tempFullNames.Length - 1] = fullName;
+                fullNames = tempFullNames;
+                tempPositions[tempPositions.Length - 1] = position;
+                positions = tempPositions;
             }
         }
 
@@ -75,58 +80,46 @@ namespace PersonnelAccounting
             {
                 Console.Write(" {0}) {1} - {2}", i + 1, fullNames[i], positions[i]);
             }
-
-            Console.ReadKey();
         }
 
-        static void OutputDossiers(string[] fullNames, string[] positions, string lastname)
+        static void DeleteDossier(ref string[] fullNames, ref string[] positions)
         {
-            string fullName = "";
+            Console.Write("Номер досье: ");
+            int dossierNumber = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = dossierNumber - 1; i < fullNames.Length - 1; i++)
+            {
+                fullNames[i] = fullNames[i + 1];
+                positions[i] = positions[i + 1];
+            }
+
+            string[] tempFullNames = new string[fullNames.Length - 1];
+            string[] tempPositions = new string[fullNames.Length - 1];
+
+            for (int i = 0; i < tempFullNames.Length; i++)
+            {
+                tempFullNames[i] = fullNames[i];
+                tempPositions[i] = positions[i];
+            }
+
+            fullNames = tempFullNames;
+            positions = tempPositions;
+        }
+
+        static string SearchByLastname(string[] fullNames, string[] positions)
+        {
+            Console.Write("Фамилия: ");
+            string lastname = Console.ReadLine();
+            string dossier = "";
 
             for (int i = 0; i < fullNames.Length; i++)
             {
-                for (int j = 0; fullNames[i][j] != ' '; j++)
-                {       
-                        fullName += fullNames[i][j];
- 
-                    if (fullName == lastname)
-                    {
-                        Console.Write("{0}) {1} - {2} ", i + 1, fullNames[i], positions[i]);
-                        fullName = "";
-                    }
-                }               
-            }
-            Console.ReadKey();
-        }
-
-        static string[] DeleteDossier(int value, string[] arrey)
-        {
-            if (arrey.Length >= value)
-            {
-                for (int i = value - 1; i < arrey.Length; i++)
+                if (fullNames[i].ToLower().Contains(lastname.ToLower()))
                 {
-                    if (i != arrey.Length - 1)
-                    {
-                        arrey[i] = arrey[i + 1];
-                    }
+                    dossier += (i + 1) + ") " + fullNames[i] + " - " + positions[i] + " ";
                 }
-
-                string[] tempArrey = new string[arrey.Length - 1];
-
-                for (int i = 0; i < arrey.Length; i++)
-                {
-                    if (i < tempArrey.Length)
-                        tempArrey[i] = arrey[i];
-                }
-
-                arrey = tempArrey;
             }
-            else
-            {
-                Console.WriteLine("Нет досье по такому номеру!");
-            }
-
-            return arrey;
+            return dossier;
         }
     }
 }
