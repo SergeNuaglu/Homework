d4df86d269139505cd66bd923bbcadb7caa87560
SergeNuaglu <serge.nuaglu@gmail.com>
2022-02-07 21:31:53 +0200

    Created UIElement

--- UIElement/Program.cs
+++ UIElement/Program.cs
@@ -6,9 +6,46 @@ namespace UIElement
     {
         public static void Main(string[] args)
         {
-            string bar;
-            int barMaxSize = 10;
+            float barMaxSize = 10f;
+            float health = 40f;
+            float maxHealth = 100f;
+            char fillSymbol = '#';
+            int barXPosition = 0;
+            int barYPosition = 0;
+            float healthPercent;
+            ConsoleColor barColor = ConsoleColor.DarkRed;
 
+            healthPercent = health / (maxHealth / 100);
+            DrawBar(healthPercent, barMaxSize, barXPosition, barYPosition, barColor, fillSymbol);
+
+        }
+
+        static void DrawBar(float fillPercent, float maxSize, int posX, int posY, ConsoleColor color, char symbol)
+        {
+            ConsoleColor defaultColor;
+            defaultColor = Console.BackgroundColor;
+            string bar = "";
+            float barFillPercent = maxSize / 100 * fillPercent;
+
+            for (int i = 0; i < barFillPercent; i++)
+            {
+                bar += symbol;
+            }
+
+            Console.SetCursorPosition(posX, posY);
+            Console.Write("[");
+            Console.BackgroundColor = color;
+            Console.Write(bar);
+            Console.BackgroundColor = defaultColor;
+
+            bar = "";
+
+            for (int i = (int)barFillPercent; i < maxSize; i++)
+            {
+                bar += " ";
+            }
+
+            Console.Write(bar + "]");
         }
     }
 }
