1cc3e51fb68e645ee2f980d202b18317f21eb886
SergeNuaglu <serge.nuaglu@gmail.com>
2022-02-08 10:15:59 +0200

    created ReadInt

---
+++ ReadInt/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ReadInt
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int number;
+
+            ConvertToInt(out number);
+            Console.WriteLine(number);
+            Console.ReadKey();
+        }
+
+        static int ConvertToInt(out int number)
+        {
+            while (Int32.TryParse(Console.ReadLine(), out number) == false)
+            {
+                Console.Clear();
+                Console.Write("Введите число: ");  
+            }
+
+            return number;
+        }
+    }
+}
