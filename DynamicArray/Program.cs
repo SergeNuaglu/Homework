fa60ab353045423809be4104bf2a0403fb466eb6
SergeNuaglu <serge.nuaglu@gmail.com>
2022-02-09 12:27:58 +0200

    solved

---
+++ DynamicArray/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SumOfNumbers
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            List<float> numbers = new List<float>();
+            string sumCommand = "sum";
+            string exitCommand = "exit";
+            float number;
+            string userInput;
+            bool isAppWork = true;
+
+            while (isAppWork)
+            {
+                Console.Clear();
+                Console.SetCursorPosition(0, 5);
+                Console.WriteLine("Команды:\nsum - сложить числа\nexit - выйти из программы\n\n");
+                Console.SetCursorPosition(0, 0);
+                Console.WriteLine("Введите число или команду: ");
+                userInput = Console.ReadLine();
+
+                if (float.TryParse(userInput,out number))
+                {
+                    numbers.Add(number);
+                }
+                else if (sumCommand == userInput)
+                {
+                    if (numbers.Count >= 2)
+                    {
+                        Console.WriteLine("Сумма чисел = " + AddNumbers(numbers));
+                    }
+                    else
+                    {
+                        Console.WriteLine("Для сложения нужно как минимум два числа!");
+                    }
+
+                    Console.ReadKey();
+                }
+                else if (exitCommand == userInput)
+                {
+                    isAppWork = false;
+                }
+            }
+        }
+
+        static float AddNumbers(List<float> numbers)
+        {
+            float sum = 0;
+
+            foreach (var number in numbers)
+            {
+                sum += number;
+            }
+
+            return sum;
+        }
+    }
+}
