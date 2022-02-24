098a519f40f9631d4cd8c1396337fd9337414bc2
SergeNuaglu <serge.nuaglu@gmail.com>
2022-02-24 10:54:17 +0200

    solved

---
+++ Amnesty/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Amnesty
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            List<Prisoner> prisoners = new List<Prisoner>();
+
+            prisoners.Add(new Prisoner("Мельницкий Алексей Викторович", "Мошенничество"));
+            prisoners.Add(new Prisoner("Остроухов Генадий Павлович", "Воровство"));
+            prisoners.Add(new Prisoner("Каровин Иван Николаевич", "Антиправительственное"));
+            prisoners.Add(new Prisoner("Таблеткин Петр Сергеевич", "Антиправительственное"));
+            prisoners.Add(new Prisoner("Ефимов Николай Артемьевич", "Наркоторговля"));
+            prisoners.Add(new Prisoner("Саитов Илья Михаилович", "Воровство"));
+            prisoners.Add(new Prisoner("Троцкий Михаил иванович", "Антиправительственное"));
+            prisoners.Add(new Prisoner("Колбасов Антон Тарасович", "Антиправительственное"));
+            prisoners.Add(new Prisoner("Сидоров Николай Петрович", "Мошенничество"));
+            prisoners.Add(new Prisoner("Тарасенко Владислав Константинович", "Антиправительственное"));
+
+            Console.WriteLine("Заключенные до амнистии:\n");
+            ShowAllPrisoners(prisoners);
+            var amnestyPrisoners = prisoners.Where(prisoner => prisoner.Crime != "Антиправительственное").ToList();
+            Console.WriteLine("\nЗаключенные после амнистии:\n");
+            ShowAllPrisoners(amnestyPrisoners);
+            Console.ReadKey();
+            
+        }
+
+        public static void ShowAllPrisoners(List<Prisoner> prisoners)
+        {
+            foreach (var prison in prisoners)
+            {
+                Console.WriteLine(prison.FullName +", преступление: "+ prison.Crime);
+            }
+        }
+    }
+
+    class Prisoner
+    {
+        public string FullName { get; private set; }
+        public string Crime { get; private set; }
+
+        public Prisoner(string fullName, string crime)
+        {
+            FullName = fullName;
+            Crime = crime;
+        }
+    }
+}
