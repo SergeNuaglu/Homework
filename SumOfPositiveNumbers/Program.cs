f8ce0c8095e84fd05e4b14a41cc9e94d49e80806
SergeNuaglu <98533549+SergeNuaglu@users.noreply.github.com>
2022-09-28 19:20:41 +0300

    Done

---
+++ SumOfPositiveNumbers/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace SumOfPositiveNumbers
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int minRandomValue = 0;
+            int maxRandomValue = 100;
+            int divider = 5;
+            int multiplicityIndicator = 0;
+            int result = 0;
+            int randomNumber;
+            Random random = new Random();
+
+            randomNumber = random.Next(minRandomValue, maxRandomValue);
+            Console.WriteLine("Рандомное число: " + randomNumber);
+
+            for (int i = 0; i <= randomNumber; i++)
+            {
+                if (i % divider == multiplicityIndicator)
+                    result += i;
+            }
+
+            Console.WriteLine("Сумма всех положительных чисел меньше " + randomNumber + " (включая число), которые кратные " + divider + ":");
+            Console.WriteLine(result);
+        }
+    }
+}
