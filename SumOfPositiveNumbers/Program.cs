65777a4274a83cb42a2fa638032d57f36bfe64ea
SergeNuaglu <98533549+SergeNuaglu@users.noreply.github.com>
2022-09-28 19:50:50 +0300

    Update

--- SumOfPositiveNumbers/Program.cs
+++ SumOfPositiveNumbers/Program.cs
@@ -8,7 +8,8 @@ namespace SumOfPositiveNumbers
         {
             int minRandomValue = 0;
             int maxRandomValue = 100;
-            int divider = 5;
+            int firstDivider = 3;
+            int secondDivider = 5;
             int multiplicityIndicator = 0;
             int result = 0;
             int randomNumber;
@@ -19,11 +20,12 @@ namespace SumOfPositiveNumbers
 
             for (int i = 0; i <= randomNumber; i++)
             {
-                if (i % divider == multiplicityIndicator)
+                if (i % firstDivider == multiplicityIndicator || i % secondDivider == multiplicityIndicator)
                     result += i;
             }
 
-            Console.WriteLine("Сумма всех положительных чисел меньше " + randomNumber + " (включая число), которые кратные " + divider + ":");
+            Console.WriteLine("Сумма всех положительных чисел меньше " + randomNumber + " (включая число)," +
+                "\nкоторые кратные " + firstDivider + " или " + secondDivider + ":");
             Console.WriteLine(result);
         }
     }
