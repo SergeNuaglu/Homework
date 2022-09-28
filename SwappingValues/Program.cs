86f27d8e970d12d201ef1499ccd4b41f447cd373
SergeNuaglu <98533549+SergeNuaglu@users.noreply.github.com>
2022-09-28 18:40:41 +0300

    Done

---
+++ SwappingValues/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace SwappingValues
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string name = "Ivanov";
+            string surname = "Igor";
+
+            Console.WriteLine("Name: " + name + "\nSurname: " + surname);
+            Swap(ref name, ref surname);
+            Console.WriteLine("Name: " + name + "\nSurname: " + surname);
+        }
+
+        private static void Swap(ref string firstValue, ref string secondValue)
+        {
+            string swappingData;
+
+            swappingData = firstValue;
+            firstValue = secondValue;
+            secondValue = swappingData;
+        }
+    }
+}
