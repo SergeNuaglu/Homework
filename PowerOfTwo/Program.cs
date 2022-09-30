c6cd721eae9d24ee8c3ab154e538768252184f93
SergeNuaglu <98533549+SergeNuaglu@users.noreply.github.com>
2022-09-30 11:44:09 +0300

    Updated

---
+++ PowerOfTwo/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace PowerOfTwo
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Random random = new Random();
+            byte randomNumber;
+            int power = 0;
+            int factor = 2;
+            int result;
+
+            randomNumber = (byte)random.Next(byte.MinValue, byte.MaxValue);
+
+            for (result = 1; result <= randomNumber; result *= factor)
+            {
+                power++;
+            }
+
+            Console.WriteLine("Заданное число - " + randomNumber +
+                "\nMинимальная степень двойки - " + power +
+                "\nЧисло, превосходящее заданное - " + result);
+        }
+    }
+}
