65adc3039477b45b358433c8aa5328c10c905112
SergeNuaglu <98533549+SergeNuaglu@users.noreply.github.com>
2022-09-30 09:55:13 +0300

    Fixed

--- Multiples/Program.cs
+++ Multiples/Program.cs
@@ -8,25 +8,20 @@ namespace Multiples
         {
             int minRandomValue = 1;
             int maxRandomValue = 27;
-            int randomNumber;
-            int minThreeDigitNumber = 100;
-            int maxThreeDigitNumber = 999;
+            int lowerRange = 100;
+            int upperRange = 999;
             int numbersCount = 0;
-            int multiple = 0;
-            bool canWork = true;
+            int step;
             Random random = new Random();
 
-            randomNumber = random.Next(minRandomValue, maxRandomValue);
-            Console.Write("Количество трехзначных натуральных чисел, которые кратны " + randomNumber + ": ");
+            step = random.Next(minRandomValue, maxRandomValue);
+            Console.Write("Количество трехзначных натуральных чисел, которые кратны " + step + ": ");
 
-            while (canWork)
+
+            for (int i = 0; i <= upperRange; i += step)
             {
-                multiple += randomNumber;
-
-                if (multiple >= minThreeDigitNumber && multiple <= maxThreeDigitNumber)
+                if (i >= lowerRange)
                     numbersCount++;
-                else if (multiple > maxThreeDigitNumber)
-                    canWork = false;
             }
 
             Console.Write(numbersCount);
