fcc7e4eb0e86d59a1b52ab2432dea565a3e9a1c3
Sergey Gvozdev <serge.nuaglu@gmail.com>
2023-10-21 23:44:12 +0300

    Solve the task “Bracket Expression”

---
+++ Bracket Expression/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Bracket_Expression
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text;
+
+            do
+            {
+                Console.Write("Введите строку из символов '(' и ')': ");
+                text = Console.ReadLine();
+            }
+            while (string.IsNullOrEmpty(text) || (text.IndexOf('(') < 0 && text.IndexOf(')') < 0));
+
+            int depth = 0;
+            int maxDepth = 0;
+
+            foreach (char symbol in text)
+            {
+                if (depth > maxDepth)
+                {
+                    maxDepth = depth;
+                }
+
+                if (symbol == '(')
+                {
+                    depth++;
+                }
+                else if (symbol == ')')
+                {
+                    depth--;
+                }
+
+                if (depth < 0)
+                {
+                    break;
+                }
+            }
+
+            if (depth != 0)
+            {
+                Console.WriteLine("Введено некорректное скобочное выражение");
+            }
+            else
+            {
+                Console.WriteLine("Введено корректное скобочное выражение, " +
+                                  $"максимальная глубина = {maxDepth}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
