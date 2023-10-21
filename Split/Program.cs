2d58563d7a191747cffce3e5de43b5a5a973c0c2
Sergey Gvozdev <serge.nuaglu@gmail.com>
2023-10-22 00:08:12 +0300

    Solve the task "Split"

---
+++ Split/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Split
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text;
+
+            do
+            {
+                Console.Write("Введите текст: ");
+                text = Console.ReadLine();
+            }
+            while (string.IsNullOrWhiteSpace(text));
+
+            string[] splitText = text.Split(' ');
+
+            foreach (string word in splitText)
+            {
+                Console.WriteLine(word);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
